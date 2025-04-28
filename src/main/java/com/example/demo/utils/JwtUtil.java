76c6ed1940a3c8b2939d508f374820d573e7015a
utkarsh-wissengt7 <Utkarsh.Singh@wissen.com>
2025-04-28 14:35:19 +0530

    renaming expirationTime to EXPIRATION_TIME

--- src/main/java/com/example/demo/utils/JwtUtil.java
+++ src/main/java/com/example/demo/utils/JwtUtil.java
@@ -14,7 +14,7 @@ import lombok.extern.slf4j.Slf4j;
 public class JwtUtil {
 
     private final Key key;
-    private static final long expirationTime = 1000 * 60 * 60; // 1 hour
+    private static final long EXPIRATION_TIME = 1000L * 60 * 60; // 1 hour
 
     public JwtUtil(@Value("${jwt.secret}") String secret) {
         this.key = Keys.hmacShaKeyFor(secret.getBytes());
@@ -25,7 +25,7 @@ public class JwtUtil {
         return Jwts.builder()
                 .setSubject(email)
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + expirationTime))
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
                 .signWith(key, SignatureAlgorithm.HS256)
                 .compact();
     }
